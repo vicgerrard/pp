e6a1139da955c77729811babc3a5009376a44462
VIKTOR <VIKBAG@fuit.spbstu.ru>
2017-05-28 17:05:59 +0300

    retarget projects

---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,19 @@
+// Lab3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "amp.h"
+#include "amp_math.h"
+#include <ctime>
+#include <iostream>
+
+using namespace concurrency;
+using namespace std;
+
+int main()
+{
+	srand(time(nullptr));
+
+    return 0;
+}
+
